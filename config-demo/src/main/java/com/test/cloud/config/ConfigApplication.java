5afc24c444ba5f93de230d767d6d594be245fd54
zhaohan <han.zhao2@b-and-qchina.com>
2017-09-08 20:30:08 +0800

    init cloud project

---
+++ config-demo/src/main/java/com/test/cloud/config/ConfigApplication.java
@@ -0,0 +1,29 @@
+package com.test.cloud.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created on 2017/9/8.
+ */
+@Configuration
+@EnableAutoConfiguration
+@RestController
+public class ConfigApplication {
+
+	@Value("${config.name}")
+	String name = "World";
+
+	@RequestMapping("/")
+	public String home() {
+		return "Hello " + name;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigApplication.class, args);
+	}
+}

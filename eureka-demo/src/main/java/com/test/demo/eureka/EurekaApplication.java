a7f481f9846431386d7e226eaf3e96b3c66d54d7
zhaohan <han.zhao2@b-and-qchina.com>
2017-09-19 15:34:22 +0800

    add eureka-demo

---
+++ eureka-demo/src/main/java/com/test/demo/eureka/EurekaApplication.java
@@ -0,0 +1,17 @@
+package com.test.demo.eureka;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * eureka访问地址： http://127.0.0.1:8080/
+ * Created on 2017/9/19.
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(EurekaApplication.class).web(true).run(args);
+	}
+}
